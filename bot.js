50103492ba79536d3cd42a108c67eb755880c78e
michaelwjones <michael.wayne.jones@gmail.com>
2016-08-17 14:05:10 -0400

    added michaels hatred for haleigh

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@
 const Telegram = require('telegram-node-bot')
 const TelegramBaseController = Telegram.TelegramBaseController
 const tg = new Telegram.Telegram('talk to @botfather to get a key')
-
+var howMadMjIsAtHaleigh = ''
 
 class ShenanigansController extends TelegramBaseController {
     /**
@@ -15,6 +15,12 @@ class ShenanigansController extends TelegramBaseController {
         }
         else if('help' == $.query.cmd){
                   $.sendMessage("I'm the helpful S! bot.  I know the following commands: calendar, help")
+        }
+        else if($.query.cmd.contains('supersecretcommand')){
+            howMadMjIsAtHaleigh = $.query.cmd.substr(str.indexOf(' ')+1) //pull out string after first space
+        }
+        else if($.query.cmd.contains('michael') && $.query.cmd.contains('haleigh') && $.query.cmd.contains('hate')){
+            $.sendMessage("Michael is currently this mad at Haleigh: " + howMadMjIsAtHaleigh)
         }
         else {
             $.sendMessage("Sorry, I don't know that command.  Keebler only spent 20 minutes on this code so I'm still dumber than a bag of doorknobs.  Tell him to get busy and add more stuff.")
